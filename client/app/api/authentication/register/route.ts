259969102c28bef5e6aa9ba0b699ca655cc7cca9
Azat Seyrek <azat.seyrek@adesso.com.tr>
2023-09-22 17:56:23 +0300

    register route complated

--- client/app/api/authentication/register/route.ts
+++ client/app/api/authentication/register/route.ts
@@ -1,8 +1,62 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export const POST = async (request: NextRequest) => {
-  const body = await request.json();
-  console.log(body.input.data);
+import { REGISTER_USER_MUTATION } from '@/graphql/mutations';
+import { IS_USER_EXIST } from '@/graphql/queries';
+import Hasura from '@/utils/clients/hasura';
+import { signAccessToken } from '@/utils/helpers';
+import bcrypt from 'bcrypt';
 
-  return NextResponse.json({ accessToken: 'accesTokenRegister' });
+interface IUsers {
+  users: {
+    id?: string;
+  }[];
+}
+
+interface INewUser {
+  insert_users_one: {
+    fullName: string;
+    email: string;
+    password: string;
+  };
+}
+
+export const POST = async (request: NextRequest) => {
+  const res = await request.json();
+  const { email, password, fullName } = res.input.data;
+
+  if (!email || !password || !fullName) {
+    return NextResponse.json({
+      accesToken: null,
+      message: 'all fields are required',
+    });
+  }
+
+  try {
+    const response = await Hasura.request(IS_USER_EXIST, {
+      email,
+    });
+
+    const { users } = response as IUsers;
+
+    if (users.length > 0) {
+      return NextResponse.json({ message: 'email already exists' });
+    } else {
+      //   return NextResponse.json({ message: 'email doesnt exists' });
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      const newUser: INewUser = await Hasura.request(REGISTER_USER_MUTATION, {
+        email,
+        password: hashedPassword,
+        fullName,
+      });
+      console.log(newUser, 'new user -------');
+      const accessToken = await signAccessToken(newUser.insert_users_one);
+      return NextResponse.json({ accessToken });
+    }
+  } catch (e) {
+    return NextResponse.json({
+      accessToken: null,
+      message: 'error for checking is email exists',
+    });
+  }
 };
--- client/utils/helpers/index.js
+++ client/utils/helpers/index.js
@@ -1,3 +1,5 @@
+import JWT from 'jsonwebtoken';
+
 export const formatRelativeDate = (dateString) => {
   const date = new Date(dateString);
   const now = new Date();
@@ -17,4 +19,30 @@ export const formatRelativeDate = (dateString) => {
   } else {
     return `${elapsedSeconds} seconds ago`;
   }
+};
+
+export const signAccessToken = (user) => {
+  return new Promise((resolve, reject) => {
+    const payload = {
+      'https://hasura.io/jwt/claims': {
+        'x-hasura-allowed-roles': ['user'],
+        'x-hasura-default-roles': 'user',
+        'x-hasura-user-id': user.id.toString(),
+      },
+      email: user.email,
+    };
+    const options = {
+      expiresIn: '1h',
+      issuer: 'adesso-okr',
+      audience: user.id.toString(),
+    };
+
+    JWT.sign(payload, process.env.JWT_SECRET, options, (err, token) => {
+      if (err) {
+        reject('jwt sign error', err);
+      } else {
+        resolve(token);
+      }
+    });
+  });
 };
